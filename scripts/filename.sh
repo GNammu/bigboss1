eaf7249742118fc65734d6308442b5203222bedb
srinadhreddy <srinadhr7@gmail.com>
2019-08-27 10:06:35 +0530

    shell scripting sample

---
+++ scripts/filename.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#########################################################
+#purpose: to learn shell scripting
+#name: srinadh
+#input: none
+#output:print some output in screen
+#version:1.0
+########################################################
+clear
+#print welcome message
+echo '------welcome to shell scripting srinadh-------'
+sleep 5 # sleep for 5 secs
+
+echo '--------------------END----------------------------'
